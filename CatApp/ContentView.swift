4e1930c9d2f397f7235c774142061c9d7d46a661
Vsevolod Pavlovskyi <pavlovskyi.ve@gmail.com>
2020-04-19 21:05:25 +0300

    initial commit

--- CatApp/ContentView.swift
+++ CatApp/ContentView.swift
@@ -9,34 +9,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var selection = 0
- 
+    @EnvironmentObject var userData: UserData
+    
     var body: some View {
-        TabView(selection: $selection){
-            Text("First View")
-                .font(.title)
-                .tabItem {
-                    VStack {
-                        Image("first")
-                        Text("First")
-                    }
-                }
-                .tag(0)
-            Text("Second View")
-                .font(.title)
-                .tabItem {
-                    VStack {
-                        Image("second")
-                        Text("Second")
-                    }
-                }
-                .tag(1)
+        NavigationView {
+            TabsView()
+                .padding()
+                .navigationBarTitle("Home")
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().environmentObject(UserData())
     }
 }
